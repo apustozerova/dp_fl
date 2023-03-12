fd58f922b9c950506f37d8260f431f51f382ccaf
Anastasia Pustozerova <pustozerova@phoibe.ifs.tuwien.ac.at>
2023-03-12 23:05:17 +0100

    purchase 32

--- fl_main.py
+++ fl_main.py
@@ -10,10 +10,10 @@ import json
 import random
 
 rand_seed = 42
-dataset = 'texas'
-# x_target_train, y_target_train, x_target_test, y_target_test = scripts.load_purchase(rand_seed)
+dataset = ''
+x_target_train, y_target_train, x_target_test, y_target_test = scripts.load_purchase(rand_seed)
 # x_target_train, y_target_train, x_target_test, y_target_test = scripts.load_loan(rand_seed, tr_size=10000)
-x_target_train, y_target_train, x_target_test, y_target_test = scripts.load_texas()
+#x_target_train, y_target_train, x_target_test, y_target_test = scripts.load_texas()
 
 #for al, l2 in [(0.001, 0.0001), (0.01, 0.0001), (0.001, 1e-5), (0.01, 1e-5)]:
 #for iter in [10, 50, 100, 200]:
@@ -22,11 +22,11 @@ for rand_seed in [42]:
     np.random.seed(rand_seed)
     random.seed(rand_seed)
 
-    #for epsilon in [0.1, 0.5, 1, 5, 10, 50, 100, 500, 1000, 5000, 10000]:
+    for epsilon in [0.1, 0.5, 1, 5, 10, 50, 100, 500, 1000, 5000, 10000]:
     #for n_clients in [64, 128]:
-    for C in [1]:
+    #for C in [2]:
 
-        number_of_clients = 2
+        number_of_clients = 4
         fl_iterations = 10
         data_per_client = int(x_target_train.shape[0]/number_of_clients)
 
@@ -37,14 +37,14 @@ for rand_seed in [42]:
 
             clients[i].n_classes      = len(np.unique(y_target_test))
             clients[i].alpha          = 0.01
-            clients[i].max_iter       = 1
+            clients[i].max_iter       = 100
             clients[i].lambda_        = 1e-6
             clients[i].tolerance      = 1e-6
-            clients[i].sgdDP          = False
-            clients[i].L              = 1 #should be 1 if DP == False
-            clients[i].epsilon        = 1
-            clients[i].C              = 1
-            clients[i].outDP_local          = True
+            clients[i].sgdDP          = True
+            clients[i].L              = 20 #should be 1 if DP == False
+            clients[i].epsilon        = epsilon
+            clients[i].C              = 2
+            clients[i].outDP_local          = False
             clients[i].outDP_local_epsilon  = 1
 #             clients[i].outDP_global         = False #not supported yet
 #             clients[i].outDP_global_epsilon = 1 #not supported yet
@@ -54,7 +54,7 @@ for rand_seed in [42]:
             clients[i].x = x_target_train[i*data_per_client:(i+1)*data_per_client]
             clients[i].y = y_target_train[i*data_per_client:(i+1)*data_per_client]
 
-        fl_path = f'{dataset}/fl/rs{rand_seed}_ncl{number_of_clients}_fiter{fl_iterations}_lr{clients[0].alpha}_iter{clients[0].max_iter}_reg{clients[0].lambda_}'
+        fl_path = f'{dataset}fl/rs{rand_seed}_ncl{number_of_clients}_fiter{fl_iterations}_lr{clients[0].alpha}_iter{clients[0].max_iter}_reg{clients[0].lambda_}'
         if clients[0].sgdDP:
             fl_path += f'_sgdDP{clients[0].sgdDP}_eps{clients[0].epsilon}_L{clients[0].L}_C{clients[0].C}'
         if clients[i].outDP_local:
